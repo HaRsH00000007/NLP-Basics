44e26a9c37b1607247bf1d25e45d0fa171f8763e
Harsh0.0.7 <124857047+HaRsH00000007@users.noreply.github.com>
2024-05-08 23:55:40 +0530

    Add files via upload

---
+++ NLP/Intro.py
@@ -0,0 +1,12 @@
+corpus="""Hello Welcome, to Harsh Singh Joyfull life.
+Please stay with me forever! I love you all of you my loved ones.
+"""
+
+#print(corpus)
+
+# TOKENIZATION
+# Converting Paragraph --> Sentences.
+
+from nltk.tokenize import sent_tokenize
+sent_tokenize(corpus)
+print(corpus)
